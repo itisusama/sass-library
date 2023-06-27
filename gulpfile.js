2df4981348db497c046b98a777d0fb26269e4992
Usama Hassan <usamahassan123@hotmail.com>
2023-06-27 03:33:25 -0700

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss')
+
+const buildStyles = () => {
+ return src('files/**/*.scss')
+.pipe(sass())
+.pipe(purgecss({ content: ['*.html'] }))
+.pipe(dest('css'))
+}
+const watchTask = () => {
+watch(['files/**/*.scss', '*.html'], buildStyles);
+}
+exports.default = series(buildStyles, watchTask);
